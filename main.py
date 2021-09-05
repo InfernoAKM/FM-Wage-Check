94b739f0ccf07995265c7459f20822017dc40359
InfernoAKM <musta_001@hotmail.co.uk>
2021-09-05 12:46:51 +0100

    Support for parsing through rtf file without edits to original file

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from player import Player
 
 def main():
-    players = create_list("wages.txt")
-    convert_to_player(players)
+    players = create_list("wages.rtf")
+    objects = convert_to_player(players)
+    # for player in objects:
+    #     print(player.__str__())
 
 def create_list(file):
     players = []
@@ -10,7 +12,9 @@ def create_list(file):
     next(file_open)
 
     for line in file_open:
-        players.append([line[1:-3].strip() for line in line[1:-3].split("|") if not line[1:-3].startswith("-")])
+        if line.strip() and 'Wage' not in line:
+            player = [line[1:-3].strip() for line in line[1:-3].split("|") if not line[1:-3].startswith("-")]
+            players.append(player)
 
     # Removes empty lines
     players = [x for x in players if x != [] and x != ['']]
@@ -20,8 +24,6 @@ def convert_to_player(players):
     player_objects = []
     for player in players:
         player_objects.append(Player(player[0], player[1], player[2]))
-    # for x in player_objects:
-    #     print(x.__str__())
     return player_objects
 
 main()
